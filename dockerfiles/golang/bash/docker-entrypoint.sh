9da87335c25bc467f72082a146c370bc9ce81f5e
Yu-Han Lin <guesslin1986@gmail.com>
2018-11-08 14:52:53 +0800

    - Add: golang-bash

---
+++ dockerfiles/golang/bash/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+# first arg is `-f` or `--some-option`
+# or there are no args
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+  # docker run bash -c 'echo hi'
+  exec bash "$@"
+fi
+
+exec "$@"
